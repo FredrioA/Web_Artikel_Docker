d488c0e9ba2bbcb29f0760cd1675659c8765b04a
FredrioA <61153730+FredrioA@users.noreply.github.com>
2021-05-12 09:31:20 +0700

    Docker commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://ci-web-artikel.herokuapp.com/';
+$config['base_url'] = 'http://103.31.250.45:7171/';
 
 /*
 |--------------------------------------------------------------------------
